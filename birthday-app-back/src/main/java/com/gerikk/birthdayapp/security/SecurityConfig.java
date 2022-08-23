55a97f0a8fc0e08819c63c008b42ee45688f6c42
Gerikk <al.blt@protonmail.com>
2022-08-23 11:03:18 +0200

    Correction Security

--- birthday-app-back/src/main/java/com/gerikk/birthdayapp/security/SecurityConfig.java
+++ birthday-app-back/src/main/java/com/gerikk/birthdayapp/security/SecurityConfig.java
@@ -1,19 +1,18 @@
 package com.gerikk.birthdayapp.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-    private final String[] PUBLIC_MATCHERS = {"/", "/adduser"};
 
     @Autowired
     private final UserDetailsService userDetailsService;
@@ -24,14 +23,26 @@ public class SecurityConfig {
     }
 
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll();
-
-
-        return http.build();
-
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
 
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/perform_login")
+                .defaultSuccessUrl("/users", true)
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                .logoutSuccessUrl("/")
+                .permitAll();
+    }
 }
